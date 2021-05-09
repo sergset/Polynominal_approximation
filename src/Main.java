f709ce79f3f4c8ab05a03e7b14a8886ef11925d9
sergset <45772288+sergset@users.noreply.github.com>
2021-05-09 20:30:29 +0200

    Adding transpose and multiply.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,28 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+
+
+
 public class Main {
+    public static float[][] transpose(float[][] a) {
+        int tmp = a.length;
+        float[][] b = new float[tmp][tmp];
+        for (int i = 0; i < tmp; i++){
+            for (int j = 0; j < tmp; j++){
+                b[j][i] = a[i][j];
+            }
+        }
+        return b;
+    }
+    public static float[][] multiply(float[][] a, float[] x) {
+        int tmp = a.length;
+        float[][] y = new float[tmp][tmp];
+        for (int i = 0; i < tmp; i++)
+            for (int j = 0; j < tmp; j++)
+                y[i][j] = a[i][j] * x[j];
+        return y;
+    }
     public static void main(String[] args) throws IOException {
         File file = new File("C:\\Users\\User\\IdeaProjects\\Polynominal approximation\\test.txt");
         Scanner sc = new Scanner(file);
@@ -13,26 +34,31 @@ public class Main {
         //System.out.print("Podaj wartość x: ");
         //int polynominalDegree = Integer.parseInt(buffer.readLine());
 
-        int n = 4;
+        int n = 5;
         int tmp = 0;
         int tmp2 = 0;
         float values[] = new float[n];
         float matrix[][] = new float[n][n];
-
-        while (sc.hasNextFloat()) {
-            matrix[tmp][1]  = sc.nextFloat();
+        while (sc.hasNext()) {
+            matrix[tmp][1]  = Float.parseFloat(sc.next());
             tmp++;
-            if (!sc.hasNextFloat() && sc.hasNextLine()) {
+            if (tmp>=n) {
                 break;
             }
         }
+        String result = Arrays
+                .stream(matrix)
+                .map(Arrays::toString)
+                .collect(Collectors.joining(System.lineSeparator()));
+        System.out.println(result);
 
         while (sc.hasNext()) {
                 values[tmp2]= Float.parseFloat(sc.next());
                 tmp2++;
         }
         sc.close();
-        System.out.println(Arrays.toString(values));
+
+        float[][] b = new float[tmp][tmp];
     for (int i=0; i<tmp; i++)
     {
         for (int j=0; j<tmp; j++)
@@ -40,12 +66,22 @@ public class Main {
                 matrix[i][j]= (float) Math.pow(matrix[i][1],j);
         }
     }
-        String result = Arrays
-                .stream(matrix)
+    b=transpose(matrix);
+        String result2 = Arrays
+                .stream(b)
                 .map(Arrays::toString)
                 .collect(Collectors.joining(System.lineSeparator()));
-        System.out.println(result);
+        System.out.println();
+        System.out.println(result2);
 
+
+  b=multiply(b,values);
+        String result3 = Arrays
+                .stream(b)
+                .map(Arrays::toString)
+                .collect(Collectors.joining(System.lineSeparator()));
+        System.out.println();
+        System.out.println(result3);
 
      /*       PrintWriter zapis = new PrintWriter("Newton Wynik.txt");
         zapis.println("Wartość w " + df.format(value) + " wynosi " + df.format(applyFormula(value, x, y, n)));
