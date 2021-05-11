5bdb137427be48a6b839b166f46f5d4bba2a2fe5
sergset <45772288+sergset@users.noreply.github.com>
2021-05-11 20:13:55 +0200

    Inverse class and test2.

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,50 @@ public class Main {
                 y[i][j] = a[i][j] * x[j];
         return y;
     }
+
+    public static String printFunction(float[] result) {
+        String wynik = "y=";
+        for (int i = result.length - 1; i >= 0; --i) {
+            float a = result[i];
+            a *= 1000;
+            a = Math.round(a);
+            a /= 1000;
+            if (i == 0) {
+                if (a < 0) {
+                    wynik += " " + a;
+                } else
+                    wynik += "+" + a;
+            } else if (i == 1) {
+                if (a < 0) {
+                    wynik += " " + a + "x ";
+                } else {
+                    if (i == result.length - 1)
+                        wynik +=  a + "x ";
+                    else wynik += " +" + a + "x ";
+                }
+            } else {
+                if (a < 0) {
+                    wynik += " " + a + "x^" + i;
+                } else {
+                    if (i == result.length - 1) wynik += " " + a + "x^" + i;
+                    else
+                        wynik += " +" + a + "x^" + i;
+                }
+            }
+        }
+        return wynik;
+    }
+    public static float[] multiply2(float[][] a, float[] x) {
+        int m = a.length;
+        int n = a[0].length;
+        if (x.length != n) throw new RuntimeException("Illegal matrix dimensions.");
+        float[] y = new float[m];
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                y[i] += a[i][j] * x[j];
+        return y;
+    }
+
     public static void main(String[] args) throws IOException {
         File file = new File("C:\\Users\\User\\IdeaProjects\\Polynominal approximation\\test.txt");
         Scanner sc = new Scanner(file);
@@ -46,46 +90,70 @@ public class Main {
                 break;
             }
         }
-        String result = Arrays
-                .stream(matrix)
-                .map(Arrays::toString)
-                .collect(Collectors.joining(System.lineSeparator()));
-        System.out.println(result);
 
         while (sc.hasNext()) {
                 values[tmp2]= Float.parseFloat(sc.next());
                 tmp2++;
         }
         sc.close();
-
-        float[][] b = new float[tmp][tmp];
-    for (int i=0; i<tmp; i++)
-    {
-        for (int j=0; j<tmp; j++)
-        {
-                matrix[i][j]= (float) Math.pow(matrix[i][1],j);
+            for (int i=0; i<tmp; i++) {
+                for (int j=0; j<tmp; j++) {
+                    matrix[i][j]= (float) Math.pow(matrix[i][1],j);
         }
     }
-    b=transpose(matrix);
-        String result2 = Arrays
-                .stream(b)
-                .map(Arrays::toString)
-                .collect(Collectors.joining(System.lineSeparator()));
-        System.out.println();
-        System.out.println(result2);
+        float[][] tmpmatrix = new float[n][n] ;
+        tmpmatrix=transpose(matrix);
 
+        float[][] tmpmatrix2 = new float[n][n];
+        tmpmatrix2=multiply(tmpmatrix,values);
 
-  b=multiply(b,values);
-        String result3 = Arrays
-                .stream(b)
-                .map(Arrays::toString)
-                .collect(Collectors.joining(System.lineSeparator()));
-        System.out.println();
-        System.out.println(result3);
+        float[][] sumS = new float[n][n];
+        for (int i=0; i<tmp; i++)
+        {
+            for (int j=0; j<tmp; j++)
+            {
+                sumS[i][0]+=tmpmatrix[i][j];
+            }
+        }
+        for (int j=0; j<tmp-1; j++)
+        {
+            for (int i=0; i<tmp; i++)
+            {
+                if (i-1>=0)
+                    sumS[i-1][j+1]=sumS[i][j];
+            }
+        }
 
-     /*       PrintWriter zapis = new PrintWriter("Newton Wynik.txt");
-        zapis.println("Wartość w " + df.format(value) + " wynosi " + df.format(applyFormula(value, x, y, n)));
-        zapis.println(result2);
-        zapis.close();*/
+        Scanner giveM = new Scanner(System.in);
+        System.out.println("Enter the degree of the polynomial: ");
+        int m = giveM.nextInt();
+        m=m+1;
+        float [][] sumS2 = new float[m][m];
+        for (int i=0; i<m; i++)
+        {
+            for (int j=0; j<m; j++)
+            {
+                sumS2[i][j]=sumS[i][j];
+            }
+        }
+        sumS2=invert(sumS2);
+
+        float[] sumT = new float[n];
+        float[] sumT2 = new float[m];
+
+        for (int i=0; i<tmp; i++)
+        {
+            for (int j=0; j<tmp; j++)
+            {
+                sumT[i]+=tmpmatrix2[i][j];
+            }
+        }
+        for(int i=0; i<m; i++)
+        {
+            sumT2[i]=sumT[i];
+        }
+        float[] result = new float[m];
+        result=multiply2(sumS2,sumT2);
+        System.out.println(printFunction(result));
     }
 }
